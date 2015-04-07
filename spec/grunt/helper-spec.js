d92ef8852feccfaadef52c98b1ba8828662259ba
Adam Meadows <adam.meadows@gmail.com>
2015-04-07 03:31:46 -0700

    Fixing my attributions after changing username

--- spec/grunt/helper-spec.js
+++ spec/grunt/helper-spec.js
@@ -1,5 +1,5 @@
 /**
- * @author Adam Meadows [@adammeadows](https://github.com/adammeadows)
+ * @author Adam Meadows [@job13er](https://github.com/job13er)
  * @copyright 2014-2015 Cyan, Inc. All rights reserved.
 */
 
--- src/init.js
+++ src/init.js
@@ -1,5 +1,5 @@
 /**
- * @author Adam Meadows [@adammeadows](https://github.com/adammeadows)
+ * @author Adam Meadows [@job13er](https://github.com/job13er)
  * @copyright 2015 Cyan, Inc. All rights reserved
  */
 
