793de56248045ac9e2d6a35ae9d60bc5328a0372
Adam Meadows <adam.meadows@gmail.com>
2015-04-22 17:56:35 -0700

    Fixed waitForPromise and removed supertest
    Removed the deprecated supertest helpers and fixed `waitForPromise`
    to properly call `.done()` after `.then()` to handle exceptions better.

--- src/test-utils.js
+++ src/test-utils.js
@@ -48,12 +48,12 @@ ns.wait = function (done, milliseconds, callback, timeoutFn) {
  * @param {Function} [callback] - will be called before done() so you can expect() stuff
  */
 ns.waitForPromise = function (done, promise, callback) {
-    promise.done(function (data) {
+    promise.then(function (data) {
         if (callback) {
             callback(data);
         }
         done();
-    });
+    }).done();
 };
 
 // ------------------------------------------------------------------------------------------------
@@ -79,49 +79,5 @@ ns.cssCallback = function (screenshots) {
         });
     };
 };
-
-// ------------------------------------------------------------------------------------------------
-// Super Test (DEPRECATED)
-// We probably don't need these anymore, since we don't have nodejs backends anymore
-// ------------------------------------------------------------------------------------------------
-
-/** supertest utilities */
-ns.supertest = {};
-
-/**
- * @typedef {Object} SuperTest.Response
- * {@link http://visionmedia.github.io/superagent/#response-properties}
- */
-
-/**
- * Handler for a supertest response (only on success)
- * @callback SuperTest.ResponseHandler
- * @param {SuperTest.Response} resp - the response from the supertest request
- */
-
-/**
- * This method is called after the supertest request responds (also supports errors)
- * @callback SuperTest.ErrorHandler
- * @param {Error} err - an error that occurred during supertest request
- * @param {SuperTest.Response} resp - the response from the supertest request
- */
-
-
-/**
- * Handy shortcut for pausing a jasmine spec until a supertest .end() method is called.
- * @param {Function} done - the done method from a jasmine spec
- * @param {SuperTest.OnEnd} callback - the fn to call when supertest responds
- * @returns {SuperTest.ErrorHandler} a function to pass to .end() that handles errors as well
- */
-ns.supertest.onEnd = function (done, callback) {
-    return function (err, resp) {
-        if (err) {
-            throw err;
-        }
-
-        callback(resp);
-        done();
-    };
-}; // onEnd //
 
 module.exports = ns;
