0f6aae98aa27eea9699213c327cb268293ee8077
Adam Meadows <adam.meadows@gmail.com>
2015-05-18 12:54:07 -0700

    Added spec with setTimeout and jasmine.clock()
    Added a more realistic unit-test for test-utils/wait that actually
    runs in the browser now.

--- spec/karma/test-utils-spec.6.js
+++ spec/karma/test-utils-spec.6.js
@@ -11,15 +11,32 @@ const beaker = require('../../src/test-utils');
 
 describe('test-utils', () => {
     beforeEach(() => {
+        jasmine.clock().install();
+    });
+
+    afterEach(() => {
+        jasmine.clock().uninstall();
     });
 
     describe('.wait()', () => {
-        it('is defined', () => {
-            expect(beaker.wait).toBeDefined();
+        let doneSpy;
+        beforeEach(() => {
+            doneSpy = jasmine.createSpy('doneSpy');
+            beaker.wait(doneSpy, 500);
         });
 
-        it('is defined (inverted)', () => {
-            expect(beaker.wait).not.toBeUndefined();
+        it('does not call done yet', () => {
+            expect(doneSpy).not.toHaveBeenCalled();
+        });
+
+        describe('after timeout passes', () => {
+            beforeEach(() => {
+                jasmine.clock().tick(500);
+            });
+
+            it('calls done', () => {
+                expect(doneSpy).toHaveBeenCalled();
+            });
         });
     });
 });
